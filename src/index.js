257c275746da4d5ba3733f96ed87675184d19b44
Erencan Karadağ <erencan@macerita.com>
2021-01-05 10:31:14 +0300

    Removed Dummy Data from index.js

--- src/index.js
+++ src/index.js
@@ -15,32 +15,32 @@ import 'normalize.css';
 const store = configureStore(); //Ahora podemos usar todos las funciones de  dispatch
 
 //Cuando llamamos unsubscribe() dejamos de  controlar el store
-const unsubscribe = store.subscribe(() => {
-  let state = store.getState();
-});
-//Algunos articulos defectos
-const itemOne = store.dispatch(
-  addExpense({
-    description: 'Water Bill',
-    amount: 270,
-    createdAt: moment().subtract('4', 'day').valueOf(),
-  })
-);
-//itemTwo es ahora un objeto de store dispatch
-const itemTwo = store.dispatch(
-  addExpense({
-    description: 'Gas Bill',
-    amount: 430,
-    createdAt: moment().subtract('7', 'day').valueOf(),
-  })
-);
-const itemThree = store.dispatch(
-  addExpense({
-    description: 'Electric Bill',
-    amount: 355,
-    createdAt: moment().subtract('3', 'day').valueOf(),
-  })
-);
+// const unsubscribe = store.subscribe(() => {
+//   let state = store.getState();
+// });
+// //Algunos articulos defectos
+// const itemOne = store.dispatch(
+//   addExpense({
+//     description: 'Water Bill',
+//     amount: 270,
+//     createdAt: moment().subtract('4', 'day').valueOf(),
+//   })
+// );
+// //itemTwo es ahora un objeto de store dispatch
+// const itemTwo = store.dispatch(
+//   addExpense({
+//     description: 'Gas Bill',
+//     amount: 430,
+//     createdAt: moment().subtract('7', 'day').valueOf(),
+//   })
+// );
+// const itemThree = store.dispatch(
+//   addExpense({
+//     description: 'Electric Bill',
+//     amount: 355,
+//     createdAt: moment().subtract('3', 'day').valueOf(),
+//   })
+// );
 
 const jsx = (
   //Para utilizar react-redux
